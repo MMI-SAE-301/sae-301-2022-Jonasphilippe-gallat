3be5cfca7f6b1d18a3af36b2c42f377be185fdef
Jonasphilippe-gallat <95093401+Jonasphilippe-gallat@users.noreply.github.com>
2022-10-23 18:36:57 +0200

    importation fichier svg + readme complété

---
+++ src/components/MontreProfil.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { Montre } from "@/types"
+withDefaults(defineProps<Montre>(), {
+
+});
+</script>
+
+<template>
+
+
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,31 @@
+<script setup lang="ts">
+import Montreface from "../components/MontreProfil.vue";
+import type { Montre } from "../types";
+const exemples: Montre[] = [{
+  semelle: "#00FFFF",
+  empeigne: "#FFFFFF",
+  pointe: "#FFFFFF",
+  oeillet: "#FFFFFF",
+  bande: "#FFFFFF",
+  languette: "#F0efFF",
+  lacet: "#000000",
+  trimestre: "#f63f96",
+},
+</script>
+
+
 <template>
   <section>
-    <h1 class="text-2xl">
-      Bonjour<code class="font-mono text-base">/src/pages/index.vue</code>
-    </h1>
+    <h1 class="text-2xl">Exemples de Montres</h1>
+    <div class="flex flex-col gap-2">
+      <div class="w-64 flex flex-row gap-10">
+        <RouterLink v-for="Exemple, index in exemples" :to="{
+          name: 'basket-exemple-data',
+          params: { data: JSON.stringify(Exemple) },
+        }">
+          <BasketProfil class="w-64" v-bind="Exemple" />
+        </RouterLink>
+      </div>
+    </div>
   </section>
 </template>
