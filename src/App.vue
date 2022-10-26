5fe354256722ff34532abf67f615175bee60f762
Jonasphilippe-gallat <95093401+Jonasphilippe-gallat@users.noreply.github.com>
2022-10-26 16:48:41 +0200

    ajout police + couleurs

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+
+
+
 <template>
   <div>
     <nav class="z-20">
@@ -63,8 +66,37 @@
   </Suspense>
 
   <footer>
-    <div class="bg-black w-full h-full">
-      <div></div>
+    <div class="bg-black w-full h-full flex flex-row justify-between">
+      <div class="p-16">
+        <router-link class="text-black font-black " to="/">
+          <p class="font-archivo font-700 text-white text-lg">TikTaK</p>
+
+        </router-link>
+      </div>
+      <div class="flex flex-col gap-10 justify-center">
+        <div class="flex flex-row gap-40  justify-center">
+          <router-link class=" " to="/Listeproduit">
+
+            <p class="font-archivo font-700 text-white">Nos produits</p>
+          </router-link>
+          <router-link class=" " to="/Login">
+
+            <p class="font-archivo font-700 text-white ml-9">Mon compte</p>
+          </router-link>
+
+          <p class="font-archivo font-700 text-white">Nous contacter</p>
+        </div>
+        <div class="flex flex-row gap-40  justify-center">
+          <p class="font-archivo font-700 text-gray-500">Mentions légales</p>
+          <p class="font-archivo font-700 text-gray-500 mr-9">Confidentialité</p>
+          <p class="font-archivo font-700 text-gray-500">Cookies</p>
+        </div>
+      </div>
+      <div class="flex flex-col p-16 gap-8">
+        <instagram />
+        <facebook />
+        <twitter />
+      </div>
 
     </div>
 
@@ -74,6 +106,10 @@
 </template>
 
 <script setup lang="ts">
+
+import instagram from "./components/design/instagram.vue"
+import twitter from "./components/design/twitter.vue"
+import facebook from "./components/design/facebook.vue"
 import { Bars3Icon } from "@heroicons/vue/20/solid";
 </script>
 
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -26,47 +26,47 @@ supabase.auth.onAuthStateChange((event, session) => {
                 <div class=" mt-24 p-3 border-neutral-50 border-2 shadow-2xl shadow-blue-700">
 
                     <div class="text-center m-6">
-                        <h1 class="text-3xl mb-2 font-semibold text-white">S'INSCRIRE</h1>
-                        <p class="font-thin text-sm text-white">Créer votre compte, afin de pouvoir
-                            réaliser
-                            toutes vos
-                            envies en personnalisant nos montre TikTak.</p>
+                        <h1 class="text-xl mb-2 font-montserrat text-white">S'INSCRIRE</h1>
+                        <p class="font-montserrat font-200 text-sm text-white">Créer votre compte pour avoir accès à
+                            toutes nos
+                            fonctionnalités</p>
                     </div>
 
                     <div class=" m-6">
                         <form class="mb-4">
                             <div class="mb-6">
-                                <label for="email" class="block mb-2 text-sm text-gray-400 ">
+                                <label for="email" class="block mb-2 font-montserrat text-sm text-gray-400 ">
                                     Address mail</label>
-                                <input type="email" name="email" id="email" placeholder="xxx@xxx.xxx"
-                                    class="w-full px-3 py-2 placeholder-gray-600 border border-gray-300 rounded-md " />
+                                <input type="email" name="email" id="email" placeholder="exemple@mail.com"
+                                    class="w-full px-3 py-2 font-montserrat text-sm placeholder-gray-600 border border-gray-300 rounded-md " />
                             </div>
                             <div class="mb-6">
-                                <button type="button" class="text-white w-full px-3 py-4 text-noirfond bg-blanctext ">SE
+                                <button type="button"
+                                    class="text-white font-montserrat w-full px-3 py-4  bg-blanctext ">SE
                                     CONNECTER</button>
                             </div>
 
                         </form>
 
                         <div class="flex flex-row justify-center gap-6 mb-20">
-                            <span class=" text-white absolute mt-8  bg-noirfond px-4 pb-6 text-blanctext">ou
+                            <span class=" text-white absolute mt-8  font-montserrat px-4 pb-6 ">ou
                                 connectez-vous
                                 avec</span>
                             <div class="w-full bg-gray-200 mt-3 h-px"></div>
                         </div>
 
                         <div class="flex flex-row gap-2">
-                            <button @click="loginFacebook()" class=" bg-blanctext text-noirfond  w-full p-2 flex flex-row justify-center gap-2 items-center
+                            <button @click="loginFacebook()" class="    w-full p-2 flex flex-row justify-center gap-2 items-center
                         rounded-sm hover:bg-beige duration-100 ease-in-out">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="30px" height="30px">
                                     <path fill="#1A1B20" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z" />
                                     <path fill="#fff"
                                         d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z" />
                                 </svg>
-                                <p class="text-white">Facebook</p>
+                                <p class="font-montserrat text-white font-600">Facebook</p>
                             </button>
                             <button @click="loginGoogle()"
-                                class="bg-blanctext text-noirfond  w-full p-2 flex flex-row justify-center gap-2 items-center rounded-sm hover:bg-beige duration-100 ease-in-out">
+                                class="  w-full p-2 flex flex-row justify-center gap-2 items-center rounded-sm hover:bg-beige duration-100 ease-in-out">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                                     class="w-5" viewBox="0 0 48 48">
                                     <defs>
@@ -81,7 +81,7 @@ supabase.auth.onAuthStateChange((event, session) => {
                                     <path clip-path="url(#b)" fill="#ffffff" d="M0 37l30-23 7.9 1L48 0v48H0z" />
                                     <path clip-path="url(#b)" fill="#ffffff" d="M48 48L17 24l-4-3 35-10z" />
                                 </svg>
-                                <p class="text-white">Google</p>
+                                <p class="text-white font-montserrat font-600">Google</p>
                             </button>
 
                         </div>
